151d982da9ee610b623261504cfaf49a0ca583a3
Raymond Jang <odschig@gmail.com>
2023-10-07 18:36:44 +1100

    initial load

---
+++ src/test/java/com/wyners/accountstatement/AccountStatmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyners.accountstatement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountStatmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
